2e0d7f43aca3e75ddcc51e6d6f1777d7a34b2077
Hszoo <trre1827151@gmail.com>
2024-08-08 13:14:24 +0900

    FEAT : 뉴스레터 생성

--- src/main/java/com/serengeti/hyu/backend/news/service/NewsService.java
+++ src/main/java/com/serengeti/hyu/backend/news/service/NewsService.java
@@ -1,4 +1,31 @@
 package com.serengeti.hyu.backend.news.service;
 
+import com.serengeti.hyu.backend.news.entity.News;
+import com.serengeti.hyu.backend.news.dto.NewsRequest;
+import com.serengeti.hyu.backend.news.repository.NewsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class NewsService {
+    private final NewsRepository newsRepository;
+
+    public String createNewsLetter(NewsRequest request) {
+
+        News news = new News();
+        news.setTitle(request.getTitle());
+        news.setCreatedDate(LocalDate.now());
+        news.setContent(request.getContent());
+        news.setLink(request.getLink());
+
+        // REVIEW 엔티티 저장
+        newsRepository.save(news);
+
+        return "뉴스레터 생성 완료";
+    }
 }
