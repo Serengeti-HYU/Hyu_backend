a8496b370801e4b61ea0a059f70a627e1682ddc7
woorimj <forestwr06@gmail.com>
2024-08-14 00:28:56 +0900

    Feat: RestControllerTest 생성, Fix: RestController url 생성시 오류 수정

--- src/main/java/com/serengeti/hyu/backend/rest/controller/RestController.java
+++ src/main/java/com/serengeti/hyu/backend/rest/controller/RestController.java
@@ -2,11 +2,11 @@ package com.serengeti.hyu.backend.rest.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.BufferedReader;
@@ -16,6 +16,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Optional;
+
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hue-activity")
@@ -36,7 +37,7 @@ public class RestController {
     @GetMapping("/recommend")
     public ResponseEntity<String> getCulturalEventInfo(
             @RequestParam(value="start_index", defaultValue = "1") Integer startIndex,
-            @RequestParam(value="end_index", defaultValue = "10") Integer endIndex,
+            @RequestParam(value="end_index", defaultValue = "50") Integer endIndex,
             @RequestParam(value="codename", required = false) String codename,
             @RequestParam(value="title", required = false) String title,
             @RequestParam(value="date", required = false) String date
@@ -46,19 +47,23 @@ public class RestController {
         String result;
 
         try {
-            // URL 생성
-            String urlStr = UriComponentsBuilder.fromHttpUrl(apiUrl + key + type + serviceName + "/" + startIndex + "/" + endIndex)
-                    .queryParamIfPresent("CODENAME", Optional.ofNullable(codename))
-                    .queryParamIfPresent("TITLE", Optional.ofNullable(title))
-                    .queryParamIfPresent("DATE", Optional.ofNullable(date))
+            // URL 생성 -> pathSegment
+            String urlStr = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                    .pathSegment(key, type, serviceName)
+                    .pathSegment(startIndex.toString(), endIndex.toString())
+                    .queryParam("CODENAME", Optional.ofNullable(codename).filter(c -> !c.isEmpty()))
+                    .queryParam("TITLE", Optional.ofNullable(title).filter(c -> !c.isEmpty()))
+                    .queryParam("DATE", Optional.ofNullable(date))
                     .build()
                     .toUriString();
+
+            System.out.println("Generated URL: " + urlStr);  // URL 출력해보기
 
             URL url = new URL(urlStr);
             urlConnection = (HttpURLConnection) url.openConnection();
 
-            urlConnection.setConnectTimeout(10000); // 10 seconds
-            urlConnection.setReadTimeout(10000);    // 10 seconds
+            urlConnection.setConnectTimeout(10000);
+            urlConnection.setReadTimeout(10000);
 
             stream = getNetworkConnection(urlConnection);
             result = readStreamToString(stream);
@@ -73,7 +78,10 @@ public class RestController {
             }
         }
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        // 응답 데이터의 형식이 JSON인지 확인 후 -> Content-Type을 application/json으로 설정
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result);
     }
 
     private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
@@ -100,4 +108,3 @@ public class RestController {
         return result.toString();
     }
 }
-
