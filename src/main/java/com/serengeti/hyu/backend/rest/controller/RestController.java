d32898ac1bb8f7380bcf5e0bbb18eba07c3f2788
woorimj <forestwr06@gmail.com>
2024-08-15 18:21:05 +0900

    Feat: 쉼활동 목록 조회기능

---
+++ src/main/java/com/serengeti/hyu/backend/rest/controller/RestController.java
@@ -0,0 +1,44 @@
+package com.serengeti.hyu.backend.rest.controller;
+
+import com.serengeti.hyu.backend.rest.dto.RestDto;
+import com.serengeti.hyu.backend.rest.repository.RestRepository;
+import com.serengeti.hyu.backend.rest.service.RestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+@RequestMapping("/hue-activity")
+public class RestController {
+
+    @Autowired
+    private RestService restService;
+
+    @Autowired
+    private RestRepository restRepository;
+
+    // 쉼활동 목록 조회
+    @GetMapping("/recommend")
+    public ResponseEntity<List<RestDto>> getCulturalEvents(
+            @RequestParam(value = "start_index", defaultValue = "1") Integer startIndex,
+            @RequestParam(value = "end_index", defaultValue = "50") Integer endIndex) {
+
+        try {
+            restService.fetchAndSaveCulturalEventInfo(startIndex, endIndex, null, null, null);
+
+            List<RestDto> response = restService.getRestData();
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Collections.emptyList());
+        }
+    }
+}
