53f088c754c71ad908b3acba5ea2e5e005d0f145
chaechaeyeon <00dreamadream@kakao.com>
2024-08-15 16:38:43 +0900

    ADD : jwt 파일 추가

---
+++ src/main/java/com/serengeti/hyu/backend/config/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.serengeti.hyu.backend.config;
+
+import com.serengeti.hyu.backend.user.entity.User;
+import com.serengeti.hyu.backend.user.repository.UserRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtTokenUtil jwtTokenUtil, UserRepository userRepository) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.userRepository = userRepository;
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private Authentication getAuthentication(String token) {
+        String username = jwtTokenUtil.getUsernameFromToken(token);
+        User user = userRepository.findByUsername(username).orElse(null);
+        if (user != null) {
+            return new UsernamePasswordAuthenticationToken(user, token);
+        }
+        return null;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        String token = resolveToken(request);
+        if (token != null) {
+            User user = userRepository.findByUsername(jwtTokenUtil.getUsernameFromToken(token)).orElse(null);
+            if (user != null && jwtTokenUtil.validateToken(token, user)) {
+                Authentication authentication = getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
