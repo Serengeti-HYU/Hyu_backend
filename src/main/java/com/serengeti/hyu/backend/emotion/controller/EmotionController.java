ab8f0bcf0eb6665fbedc438e1d0ff94d8e914df2
soeun <so84392091@daum.net>
2024-08-19 22:38:31 +0900

    Feat: 사용자 감정기록 작성

--- src/main/java/com/serengeti/hyu/backend/emotion/controller/EmotionController.java
+++ src/main/java/com/serengeti/hyu/backend/emotion/controller/EmotionController.java
@@ -2,21 +2,22 @@ package com.serengeti.hyu.backend.emotion.controller;
 
 import com.serengeti.hyu.backend.emotion.dto.EmotionDto;
 import com.serengeti.hyu.backend.emotion.entity.Emotion;
+import com.serengeti.hyu.backend.emotion.exception.EmotionAlreadyExistsException;
 import com.serengeti.hyu.backend.emotion.service.EmotionService;
+import com.serengeti.hyu.backend.user.entity.User;
+import com.serengeti.hyu.backend.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.serengeti.hyu.backend.emotion.exception.EmotionAlreadyExistsException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hue-records")
@@ -25,57 +26,39 @@ public class EmotionController {
     @Autowired
     private EmotionService emotionService;
 
-    @PostMapping("/{userId}")
-    public ResponseEntity<Emotion> createEmotion(@PathVariable Long userId, @RequestBody EmotionDto request) {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<Emotion> createEmotion(@RequestBody EmotionDto request, @AuthenticationPrincipal User user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // 사용자 인증 실패
+        }
+        Long userId = user.getUserId();
         Emotion emotion = emotionService.createEmotion(userId, request);
         return ResponseEntity.ok(emotion);
     }
 
-//    @PostMapping("/{userId}")
-//    public ResponseEntity<Emotion> createEmotion(
-//            @PathVariable Long userId,
-//            @RequestParam("emotionImg") MultipartFile file,
-//            @RequestParam("content") String content
-//    ) {
-//        // 파일 저장 로직
-//        String imageUrl = saveImage(file); // 파일을 서버에 저장하고 경로 반환
-//
-//        EmotionDto request = new EmotionDto();
-//        request.setEmotionImg(imageUrl);
-//        request.setContent(content);
-//
-//        Emotion emotion = emotionService.createEmotion(userId, request);
-//        return ResponseEntity.ok(emotion);
-//    }
-//
-//    public String saveImage(MultipartFile file) {
-//        try {
-//            // 파일 저장 디렉토리 설정
-//            String directoryPath = "path/to/save/images/";
-//            String fileName = file.getOriginalFilename();
-//            File dest = new File(directoryPath + fileName);
-//            file.transferTo(dest);
-//            return directoryPath + fileName; // 이미지 경로 반환
-//        } catch (IOException e) {
-//            throw new RuntimeException("Failed to store file", e);
-//        }
-//    }
-
-
-    @PatchMapping("/{userId}/{recordId}")
-    public ResponseEntity<Emotion> updateEmotion(@PathVariable Long userId, @PathVariable int recordId, @RequestBody EmotionDto request) {
+    @PatchMapping("/{recordId}")
+    public ResponseEntity<Emotion> updateEmotion(@PathVariable int recordId, @RequestBody EmotionDto request, @AuthenticationPrincipal User user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // 사용자 인증 실패
+        }
+        Long userId = user.getUserId();
         Emotion emotion = emotionService.updateEmotion(userId, recordId, request);
         return ResponseEntity.ok(emotion);
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<List<Emotion>> getEmotions(@PathVariable Long userId, @RequestParam(required = false) String date) {
+    @GetMapping
+    public ResponseEntity<List<Emotion>> getEmotions(@RequestParam(required = false) String date, @AuthenticationPrincipal User user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // 사용자 인증 실패
+        }
+        Long userId = user.getUserId();
         Date queryDate;
         if (date == null) {
-            // 날짜가 지정되지 않은 경우 오늘 날짜로 설정
-            queryDate = new Date();
+            queryDate = new Date(); // 날짜가 지정되지 않은 경우 오늘 날짜로 설정
         } else {
-            // 지정된 날짜 파싱
             try {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
                 queryDate = dateFormat.parse(date);
@@ -88,8 +71,12 @@ public class EmotionController {
         return ResponseEntity.ok(emotions);
     }
 
-    @GetMapping("/{userId}/detail")
-    public ResponseEntity<Emotion> getEmotionByDate(@PathVariable Long userId, @RequestParam String date) {
+    @GetMapping("/detail")
+    public ResponseEntity<Emotion> getEmotionByDate(@RequestParam String date, @AuthenticationPrincipal User user) {
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); // 사용자 인증 실패
+        }
+        Long userId = user.getUserId();
         Date queryDate;
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -106,10 +93,9 @@ public class EmotionController {
         return ResponseEntity.ok(emotion);
     }
 
-
+    // 예외 처리
     @ExceptionHandler(EmotionAlreadyExistsException.class)
     public ResponseEntity<String> handleEmotionAlreadyExistsException(EmotionAlreadyExistsException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
-
 }
